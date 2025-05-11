470bcc50ac8edd254b83319026300f7d9c358b62
Ngulefeh Fuanyi <165861906+FuanyiKN@users.noreply.github.com>
2025-05-11 14:27:54 -0400

    Create DatabaseConnection.java

---
+++ src/main/java/com/febs24/ticketing/DatabaseConnection.java
@@ -0,0 +1,38 @@
+package com.febs24.ticketing;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/febssystem"; //Replace febssystem with the name of your db.
+    private static final String DATABASE_USER = "root";
+    private static final String DATABASE_PASSWORD = "******"; //Replace with actual password
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Failed to get a connection to the database.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static PreparedStatement prepareStatement(String sql) {
+        try {
+            Connection connection = getConnection();
+            if (connection != null) {
+                return connection.prepareStatement(sql);
+            } else {
+                System.out.println("Failed to prepare statement. No connection to the database.");
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to prepare statement.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
